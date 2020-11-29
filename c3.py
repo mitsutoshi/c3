a5a738b5c5c1c07ca589cd997ce5134580ea4298
mitsutoshi <mitsu3204@gmail.com>
2020-11-29 20:44:09 +0900

    Updated

--- c3.py
+++ c3.py
@@ -14,6 +14,7 @@ from config import targets
 class Spider(metaclass=ABCMeta):
 
     def __init__(self, name):
+        self.name = name
         ex_class = getattr(ccxt, name)
         self.ex = ex_class()
 
@@ -22,6 +23,8 @@ class Spider(metaclass=ABCMeta):
         res = self.ex.fetch_ticker(symbol=symbol)
         #print(res)
         return {
+                'name': self.name,
+                'symbol': symbol,
                 'datetime': res['datetime'],
                 'last': res['last'],
                 'bid': res['bid'],
@@ -87,6 +90,7 @@ def main():
     dbpass = os.getenv('DB_PASS', '')
 
     points = []
+    tickers = []
     for name, symbols in targets.items():
 
         if name in ex_classes:
@@ -94,7 +98,10 @@ def main():
 
         for s in symbols:
             try:
+
                 t = ex.get_ticker(s['symbol'])
+                tickers.append(t)
+
                 p = {
                     'measurement': 'prices',
                     'time': t['datetime'],
@@ -112,11 +119,84 @@ def main():
                 }
                 print(p)
                 points.append(p)
+
             except Exception as e:
                 print(e)
 
+    btcjpy = [t for t in tickers if t['symbol'] == 'BTC/JPY']
+
+    min_ask = find_min_ask(btcjpy)
+    max_ask = find_max_ask(btcjpy)
+    min_bid = find_min_bid(btcjpy)
+    max_bid = find_max_bid(btcjpy)
+
+    print('Min Ask: ', min_ask[0], min_ask[1])
+    print('Max Ask: ', max_ask[0], max_ask[1])
+    print('Min Bid: ', min_bid[0], min_bid[1])
+    print('Max Bid: ', max_bid[0], max_bid[1])
+
+    ask_dif = max_ask[1] - min_ask[1]
+    bid_dif = max_bid[1] - min_bid[1]
+    print(f'Ask diff: {ask_dif}, Bid diff: {bid_dif}')
+
+    if max_bid[1] > min_ask[1]:
+        print(f'Found a arbitrage chance: {max_bid[1] - min_ask[1]}')
+        points.append({
+            'measurement': 'price_diff',
+            'tags': {
+                'symbol': 'BTC/JPY'
+            },
+            'fields': {
+                'value': max_bid[1] - min_ask[1],
+                'buy_exchange': min_ask[0],
+                'buy_price': min_ask[1],
+                'sell_exchange': max_bid[0],
+                'sell_price': max_bid[1],
+            }
+        })
+
     client = InfluxDBClient(dbhost, dbport, dbuser, dbpass, dbname)
     client.write_points(points)
+
+
+def find_min_ask(tickers):
+    name  = ''
+    p = 0
+    for t in tickers:
+        if not p or t['ask'] < p:
+            name = t['name']
+            p = t['ask']
+    return (name, p,)
+
+
+def find_max_ask(tickers):
+    name  = ''
+    p = 0
+    for t in tickers:
+        if not p or t['ask'] > p:
+            name = t['name']
+            p = t['ask']
+    return (name, p,)
+
+
+def find_min_bid(tickers):
+    name  = ''
+    p = 0
+    for t in tickers:
+        if not p or t['bid'] < p:
+            name = t['name']
+            p = t['bid']
+    return (name, p,)
+
+
+def find_max_bid(tickers):
+    name  = ''
+    p = 0
+    for t in tickers:
+        if not p or t['bid'] > p:
+            name = t['name']
+            p = t['bid']
+    return (name, p,)
 
 
 if __name__ == '__main__':
