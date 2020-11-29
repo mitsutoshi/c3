f070fcb53cfec1f0e5b31128c3e915d012f9a8c6
mitsutoshi <mitsu3204@gmail.com>
2020-11-29 18:52:16 +0900

    Update

--- c3.py
+++ c3.py
@@ -1,11 +1,82 @@
-#! /usr/env python
+#! /usr/bin/env python
 
 import sys
 import os
+from abc import ABCMeta, abstractmethod
+from typing import Dict
+
 import ccxt
 from influxdb import InfluxDBClient
+
 from config import targets
 
+
+class Spider(metaclass=ABCMeta):
+
+    def __init__(self, name):
+        ex_class = getattr(ccxt, name)
+        self.ex = ex_class()
+
+    @abstractmethod
+    def get_ticker(self, symbol: str) -> Dict[str, float]:
+        res = self.ex.fetch_ticker(symbol=symbol)
+        #print(res)
+        return {
+                'datetime': res['datetime'],
+                'last': res['last'],
+                'bid': res['bid'],
+                'ask': res['ask'],
+                'volume': res['baseVolume'],
+                }
+
+
+class Bitflyer(Spider):
+
+    def __init__(self):
+        super().__init__('bitflyer')
+
+    def get_ticker(self, symbol: str) -> Dict[str, float]:
+        return super().get_ticker(symbol)
+
+
+class Bitbank(Spider):
+
+    def __init__(self):
+        super().__init__('bitbank')
+
+    def get_ticker(self, symbol: str) -> Dict[str, float]:
+        return super().get_ticker(symbol)
+
+
+class Liquid(Spider):
+
+    def __init__(self):
+        super().__init__('liquid')
+
+    def get_ticker(self, symbol: str) -> Dict[str, float]:
+        return super().get_ticker(symbol)
+
+
+class Coincheck(Spider):
+
+    def __init__(self):
+        super().__init__('coincheck')
+
+    def get_ticker(self, symbol: str) -> Dict[str, float]:
+        return super().get_ticker(symbol)
+
+
+ex_classes = {
+        'bitflyer': Bitflyer,
+        'bitbank': Bitbank,
+        'liquid': Liquid,
+        'coincheck': Coincheck
+        }
+
+Spider.register(Bitflyer)
+Spider.register(Bitbank)
+Spider.register(Liquid)
+Spider.register(Coincheck)
 
 def main():
 
@@ -17,24 +88,32 @@ def main():
 
     points = []
     for name, symbols in targets.items():
-        ex_class = getattr(ccxt, name)
-        ex = ex_class()
+
+        if name in ex_classes:
+            ex = ex_classes[name]()
+
         for s in symbols:
-            res = ex.fetch_ticker(symbol=s['symbol'])
-            print(f"{res['datetime']},{name},{s['symbol']},{s['currency']},{res['last']}")
-            points.append({
-                'measurement': 'prices',
-                'time': res['datetime'],
-                'tags': {
-                    'exchange': name,
-                    'symbol': s['symbol'],
-                    'currency': s['currency']
-                },
-                'fields': {
-                    'value': res['last'],
-                    'volume': res['baseVolume']
+            try:
+                t = ex.get_ticker(s['symbol'])
+                p = {
+                    'measurement': 'prices',
+                    'time': t['datetime'],
+                    'tags': {
+                        'exchange': name,
+                        'symbol': s['symbol'],
+                        'currency': s['currency']
+                    },
+                    'fields': {
+                        'last': t['last'],
+                        'ask': t['ask'],
+                        'bid': t['bid'],
+                        'volume': t['volume']
+                    }
                 }
-            })
+                print(p)
+                points.append(p)
+            except Exception as e:
+                print(e)
 
     client = InfluxDBClient(dbhost, dbport, dbuser, dbpass, dbname)
     client.write_points(points)
@@ -42,3 +121,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
--- config.py
+++ config.py
@@ -1,28 +1,32 @@
 targets = {
-        'bitfinex': [
-            {'symbol': 'BTC/JPY', 'currency': 'JPY'},
-            {'symbol': 'BTC/USD', 'currency': 'USD'},
-            {'symbol': 'ETH/JPY', 'currency': 'JPY'},
-            {'symbol': 'ETH/USD', 'currency': 'USD'},
-            {'symbol': 'XRP/USD', 'currency': 'USD'},
-            ],
+
+#        'bitfinex': [
+#            {'symbol': 'BTC/JPY', 'currency': 'JPY'},
+#            {'symbol': 'BTC/USD', 'currency': 'USD'},
+#            {'symbol': 'ETH/JPY', 'currency': 'JPY'},
+#            {'symbol': 'ETH/USD', 'currency': 'USD'},
+#            {'symbol': 'XRP/USD', 'currency': 'USD'},
+#            ],
         'bitflyer': [
-            {'symbol': 'FX_BTC_JPY', 'currency': 'JPY'},
+#            {'symbol': 'FX_BTC_JPY', 'currency': 'JPY'},
             {'symbol': 'BTC/JPY', 'currency': 'JPY'},
-            {'symbol': 'BTC/USD', 'currency': 'USD'},
-            {'symbol': 'ETH/JPY', 'currency': 'JPY'},
+#            {'symbol': 'BTC/USD', 'currency': 'USD'},
+#            {'symbol': 'ETH/JPY', 'currency': 'JPY'},
             ],
-        'bitmex': [
-            {'symbol': 'BTC/USD', 'currency': 'USD'},
-            {'symbol': 'ETH/USD', 'currency': 'USD'},
-            ],
+#        'bitmex': [
+#            {'symbol': 'BTC/USD', 'currency': 'USD'},
+#            {'symbol': 'ETH/USD', 'currency': 'USD'},
+#            ],
         'bitbank': [
             {'symbol': 'BTC/JPY', 'currency': 'JPY'},
-            {'symbol': 'BCH/JPY', 'currency': 'JPY'},
-            {'symbol': 'XRP/JPY', 'currency': 'JPY'},
-            {'symbol': 'MONA/JPY', 'currency': 'JPY'},
+#            {'symbol': 'BCH/JPY', 'currency': 'JPY'},
+#            {'symbol': 'XRP/JPY', 'currency': 'JPY'},
+#            {'symbol': 'MONA/JPY', 'currency': 'JPY'},
             ],
         'coincheck': [
+            {'symbol': 'BTC/JPY', 'currency': 'JPY'},
+            ],
+        'liquid': [
             {'symbol': 'BTC/JPY', 'currency': 'JPY'},
             ],
         }
