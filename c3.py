41f143db83e42736e2364abacd6d00ed3675e030
mitsutoshi <mitsu3204@gmail.com>
2021-05-22 20:55:33 +0900

    Update

--- c3.py
+++ c3.py
@@ -5,109 +5,64 @@ import os
 from abc import ABCMeta, abstractmethod
 from typing import Dict
 
-import ccxt
 from influxdb import InfluxDBClient
 
 from config import targets
 
+from exchanges import ex_classes
 
-class Spider(metaclass=ABCMeta):
 
-    def __init__(self, name):
-        self.name = name
-        ex_class = getattr(ccxt, name)
-        self.ex = ex_class()
 
-    @abstractmethod
-    def get_ticker(self, symbol: str) -> Dict[str, float]:
-        res = self.ex.fetch_ticker(symbol=symbol)
-        #print(res)
+def arbit(tickers, symbol: str):
+
+    sym = norm_symbol(symbol)
+    tkrs = [t for t in tickers if norm_symbol(t['symbol']) == sym]
+
+    min_ask_ex, min_ask_price = find_min_ask(tkrs)
+    max_bid_ex, max_bid_price = find_max_bid(tkrs)
+    print('Min Ask: ', min_ask_ex, min_ask_price, 'Max Bid: ', max_bid_ex, max_bid_price)
+
+    if max_bid_price > min_ask_price:
+        print(f'Found a chance: symbol={sym}, width={(max_bid_price - min_ask_price):.2f}, exchange={min_ask_ex}(ask),{max_bid_ex}(bid)')
         return {
-                'name': self.name,
-                'symbol': symbol,
-                'datetime': res['datetime'],
-                'last': res['last'],
-                'bid': res['bid'],
-                'ask': res['ask'],
-                'volume': res['baseVolume'],
-                }
-
-
-class Bitflyer(Spider):
-
-    def __init__(self):
-        super().__init__('bitflyer')
-
-    def get_ticker(self, symbol: str) -> Dict[str, float]:
-        return super().get_ticker(symbol)
-
-
-class Bitbank(Spider):
-
-    def __init__(self):
-        super().__init__('bitbank')
-
-    def get_ticker(self, symbol: str) -> Dict[str, float]:
-        return super().get_ticker(symbol)
-
-
-class Liquid(Spider):
-
-    def __init__(self):
-        super().__init__('liquid')
-
-    def get_ticker(self, symbol: str) -> Dict[str, float]:
-        return super().get_ticker(symbol)
-
-
-class Coincheck(Spider):
-
-    def __init__(self):
-        super().__init__('coincheck')
-
-    def get_ticker(self, symbol: str) -> Dict[str, float]:
-        return super().get_ticker(symbol)
-
-
-ex_classes = {
-        'bitflyer': Bitflyer,
-        'bitbank': Bitbank,
-        'liquid': Liquid,
-        'coincheck': Coincheck
+            'measurement': 'price_diff',
+            'tags': {
+                'symbol': sym
+            },
+            'fields': {
+                'value': max_bid_price - min_ask_price,
+                'buy_exchange': min_ask_ex,
+                'buy_price': min_ask_price,
+                'sell_exchange': max_bid_ex,
+                'sell_price': max_bid_price,
+            }
         }
+    return {}
 
-Spider.register(Bitflyer)
-Spider.register(Bitbank)
-Spider.register(Liquid)
-Spider.register(Coincheck)
 
 def main():
 
-    dbhost = os.environ['DB_HOST']
-    dbport = os.environ['DB_PORT']
-    dbname = os.environ['DB_NAME']
-    dbuser = os.getenv('DB_USER', '')
-    dbpass = os.getenv('DB_PASS', '')
-
     points = []
     tickers = []
+
     for name, symbols in targets.items():
 
+        # get exchange class
         if name in ex_classes:
             ex = ex_classes[name]()
+        else:
+            continue
 
         for s in symbols:
             try:
-
                 t = ex.get_ticker(s['symbol'])
                 tickers.append(t)
-
                 p = {
                     'measurement': 'prices',
                     'time': t['datetime'],
                     'tags': {
                         'exchange': name,
-                        'symbol': s['symbol'],
+                        'symbol': norm_symbol(s['symbol']),
                         'currency': s['currency']
                     },
                     'fields': {
@@ -123,80 +78,41 @@ def main():
             except Exception as e:
                 print(e)
 
-    btcjpy = [t for t in tickers if t['symbol'] == 'BTC/JPY']
+    btcjpy = arbit(tickers, 'BTC/JPY')
+    xrpjpy = arbit(tickers, 'XRP/JPY')
 
-    min_ask = find_min_ask(btcjpy)
-    max_ask = find_max_ask(btcjpy)
-    min_bid = find_min_bid(btcjpy)
-    max_bid = find_max_bid(btcjpy)
+    if btcjpy:
+        points.append(btcjpy)
+    if xrpjpy:
+        points.append(xrpjpy) 
 
-    print('Min Ask: ', min_ask[0], min_ask[1])
-    print('Max Ask: ', max_ask[0], max_ask[1])
-    print('Min Bid: ', min_bid[0], min_bid[1])
-    print('Max Bid: ', max_bid[0], max_bid[1])
-
-    ask_dif = max_ask[1] - min_ask[1]
-    bid_dif = max_bid[1] - min_bid[1]
-    print(f'Ask diff: {ask_dif}, Bid diff: {bid_dif}')
-
-    if max_bid[1] > min_ask[1]:
-        print(f'Found a arbitrage chance: {max_bid[1] - min_ask[1]}')
-        points.append({
-            'measurement': 'price_diff',
-            'tags': {
-                'symbol': 'BTC/JPY'
-            },
-            'fields': {
-                'value': max_bid[1] - min_ask[1],
-                'buy_exchange': min_ask[0],
-                'buy_price': min_ask[1],
-                'sell_exchange': max_bid[0],
-                'sell_price': max_bid[1],
-            }
-        })
-
-    client = InfluxDBClient(dbhost, dbport, dbuser, dbpass, dbname)
+    dbhost = os.environ['DB_HOST']
+    dbport = os.environ['DB_PORT']
+    dbname = os.environ['DB_NAME']
+    client = InfluxDBClient(dbhost, dbport, '', '', dbname)
     client.write_points(points)
 
 
 def find_min_ask(tickers):
-    name  = ''
-    p = 0
-    for t in tickers:
-        if not p or t['ask'] < p:
-            name = t['name']
-            p = t['ask']
-    return (name, p,)
-
-
-def find_max_ask(tickers):
-    name  = ''
-    p = 0
-    for t in tickers:
-        if not p or t['ask'] > p:
-            name = t['name']
-            p = t['ask']
-    return (name, p,)
-
-
-def find_min_bid(tickers):
-    name  = ''
-    p = 0
-    for t in tickers:
-        if not p or t['bid'] < p:
-            name = t['name']
-            p = t['bid']
-    return (name, p,)
+    price = min([t['ask'] for t in tickers])
+    name = [t['name'] for t in tickers if t['ask'] == price][0]
+    return (name, price,)
 
 
 def find_max_bid(tickers):
-    name  = ''
-    p = 0
-    for t in tickers:
-        if not p or t['bid'] > p:
-            name = t['name']
-            p = t['bid']
-    return (name, p,)
+    price = max([t['bid'] for t in tickers])
+    name = [t['name'] for t in tickers if t['bid'] == price][0]
+    return (name, price,)
+
+
+def norm_symbol(symbol: str) -> str:
+    if symbol == 'BTC_JPY':
+        return 'BTC/JPY'
+    elif symbol == 'ETH_JPY':
+        return 'ETH/JPY'
+    elif symbol == 'XRP_JPY':
+        return 'XEP/JPY'
+    return symbol
 
 
 if __name__ == '__main__':
--- config.py
+++ config.py
@@ -1,32 +1,27 @@
 targets = {
-
-#        'bitfinex': [
-#            {'symbol': 'BTC/JPY', 'currency': 'JPY'},
-#            {'symbol': 'BTC/USD', 'currency': 'USD'},
-#            {'symbol': 'ETH/JPY', 'currency': 'JPY'},
-#            {'symbol': 'ETH/USD', 'currency': 'USD'},
-#            {'symbol': 'XRP/USD', 'currency': 'USD'},
-#            ],
         'bitflyer': [
-#            {'symbol': 'FX_BTC_JPY', 'currency': 'JPY'},
             {'symbol': 'BTC/JPY', 'currency': 'JPY'},
-#            {'symbol': 'BTC/USD', 'currency': 'USD'},
-#            {'symbol': 'ETH/JPY', 'currency': 'JPY'},
+            {'symbol': 'ETH/JPY', 'currency': 'JPY'},
+            {'symbol': 'XRP/JPY', 'currency': 'JPY'},
             ],
-#        'bitmex': [
-#            {'symbol': 'BTC/USD', 'currency': 'USD'},
-#            {'symbol': 'ETH/USD', 'currency': 'USD'},
-#            ],
         'bitbank': [
             {'symbol': 'BTC/JPY', 'currency': 'JPY'},
-#            {'symbol': 'BCH/JPY', 'currency': 'JPY'},
-#            {'symbol': 'XRP/JPY', 'currency': 'JPY'},
-#            {'symbol': 'MONA/JPY', 'currency': 'JPY'},
+            {'symbol': 'ETH/JPY', 'currency': 'JPY'},
+            {'symbol': 'XRP/JPY', 'currency': 'JPY'},
             ],
         'coincheck': [
             {'symbol': 'BTC/JPY', 'currency': 'JPY'},
+            {'symbol': 'ETH/JPY', 'currency': 'JPY'},
+            {'symbol': 'XRP/JPY', 'currency': 'JPY'},
             ],
         'liquid': [
             {'symbol': 'BTC/JPY', 'currency': 'JPY'},
+            {'symbol': 'ETH/JPY', 'currency': 'JPY'},
+            {'symbol': 'XRP/JPY', 'currency': 'JPY'},
+            ],
+        'gmo': [
+            {'symbol': 'BTC_JPY', 'currency': 'JPY'},
+            {'symbol': 'ETH_JPY', 'currency': 'JPY'},
+            {'symbol': 'XRP_JPY', 'currency': 'JPY'},
             ],
         }
